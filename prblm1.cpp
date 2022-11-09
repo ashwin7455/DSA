f3e10a659c39f775df939964aac266131a71b6ad
Your Name <you@example.com>
2022-11-09 22:29:28 +0530

    prblm1

---
+++ prblm1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int firstOcc(int arr[], int n, int key){
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e){
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid-1;
+        }
+        else if (key > arr[mid]){ //right wale part me jao
+          s = mid + 1;
+        }
+        else if (key < arr[mid]){ //left wale part me jao   
+
+        }
+        mid = s + (e-s)/2;
+
+        return ans;
+
+    }
+}
+
+int main(){
+    int even[5] = {1,2,3,3,5};
+   
+    cout<< " first occurrence of 3 is " << firstOcc(even,5,3)<<endl;
+
+
+return 0;
+}
