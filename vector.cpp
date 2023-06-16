055bfecfed4652646edc1181bf468012b36d349a
ashwin7455 <ashwinyaadav@gmail.com>
2023-06-17 01:52:30 +0530

    loop in vector

---
+++ vector.cpp
@@ -0,0 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+   
+       vector<int> v;
+       cout<<"size: " <<v.size()<<endl;
+       cout<<"capacity:"<<v.capacity()<<endl;
+       
+       v.push_back(1);
+         cout<<"size: " <<v.size()<<endl;
+       cout<<"capacity:"<<v.capacity()<<endl;
+       
+       v.push_back(2);
+         cout<<"size: " <<v.size()<<endl;
+       cout<<"capacity:"<<v.capacity()<<endl;
+       
+       v.push_back(3);
+         cout<<"size: " <<v.size()<<endl;
+       cout<<"capacity:"<<v.capacity()<<endl;
+       
+       v.resize(5);
+         cout<<"size: " <<v.size()<<endl;
+       cout<<"capacity:"<<v.capacity()<<endl;
+       
+       v.resize(10);
+         cout<<"size: " <<v.size()<<endl;
+       cout<<"capacity:"<<v.capacity()<<endl;
+       
+       
+       
+
+    return 0;
+}
